2f52901b972fdd596eecbc464cd392bd641bd6f0
tanbingjian <tanbingjian@maoguang.org>
2016-09-02 10:18:32 +0800

    添加文件MD5计算工具

--- src/main/java/teclan/utils/FileUtils.java
+++ src/main/java/teclan/utils/FileUtils.java
@@ -5,9 +5,13 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.security.MessageDigest;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 
 import org.apache.tika.config.TikaConfig;
@@ -289,4 +293,56 @@ public class FileUtils {
         return false;
     }
 
+    public static String getFileMD5(File file) {
+        if (!file.isFile()) {
+            return null;
+        }
+        MessageDigest digest = null;
+        FileInputStream in = null;
+        byte buffer[] = new byte[1024];
+        int len;
+        try {
+            digest = MessageDigest.getInstance("MD5");
+            in = new FileInputStream(file);
+            while ((len = in.read(buffer, 0, 1024)) != -1) {
+                digest.update(buffer, 0, len);
+            }
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+        BigInteger bigInt = new BigInteger(1, digest.digest());
+        return bigInt.toString(16);
+    }
+
+    /**
+     * 获取文件夹中文件的MD5值
+     * 
+     * @param file
+     * @param listChild
+     *            ：true 递归子目录中的文件
+     * @return
+     */
+    public static Map<String, String> getDirMD5(File file, boolean listChild) {
+        if (!file.isDirectory()) {
+            return null;
+        }
+        // <filepath,md5>
+        Map<String, String> map = new HashMap<String, String>();
+        String md5;
+        File files[] = file.listFiles();
+        for (int i = 0; i < files.length; i++) {
+            File f = files[i];
+            if (f.isDirectory() && listChild) {
+                map.putAll(getDirMD5(f, listChild));
+            } else {
+                md5 = getFileMD5(f);
+                if (md5 != null) {
+                    map.put(f.getPath(), md5);
+                }
+            }
+        }
+        return map;
+    }
 }
