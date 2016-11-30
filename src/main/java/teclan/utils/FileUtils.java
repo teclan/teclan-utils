2578fd0ebf97bd116863d06f2a2942173dce2ae6
tanbingjian <tanbingjian@maoguang.org>
2016-11-30 11:38:46 +0800

    添加文件压缩工具

--- src/main/java/teclan/utils/FileUtils.java
+++ src/main/java/teclan/utils/FileUtils.java
@@ -1,9 +1,13 @@
 package teclan.utils;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigInteger;
 import java.security.MessageDigest;
@@ -16,6 +20,8 @@ import org.apache.tika.config.TikaConfig;
 import org.apache.tika.io.TikaInputStream;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.mime.MediaType;
+import org.apache.tools.zip.ZipEntry;
+import org.apache.tools.zip.ZipOutputStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -320,5 +326,85 @@ public class FileUtils {
         }
         BigInteger bigInt = new BigInteger(1, digest.digest());
         return bigInt.toString(16);
+    }
+
+    /**
+     * 压缩目录 {@code root} 至文件 {@code target}.zip
+     * 
+     * @param root
+     *            压缩目录
+     * @param target
+     *            压缩后的文件名
+     * @throws FileNotFoundException
+     */
+    public static void zip(String root, String target)
+            throws FileNotFoundException {
+        File zipDir;
+        ZipOutputStream zipOut;
+        zipDir = new File(root);
+        String zipFileName = target + ".zip";
+        try {
+            zipOut = new ZipOutputStream(new BufferedOutputStream(
+                    new FileOutputStream(zipFileName)));
+            zipOut.setEncoding("GBK");
+            zipDir(root, zipDir, zipOut);
+            zipOut.close();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
+    /**
+     * 压缩目录 {@code root} 至文件 {@code root}.zip
+     * 
+     * @param root
+     *            压缩目录
+     * @throws FileNotFoundException
+     */
+    public static void zip(String root) throws FileNotFoundException {
+        File zipDir;
+        ZipOutputStream zipOut;
+        zipDir = new File(root);
+        String zipFileName = zipDir.getName() + ".zip";
+        try {
+            zipOut = new ZipOutputStream(new BufferedOutputStream(
+                    new FileOutputStream(zipFileName)));
+            zipOut.setEncoding("GBK");
+            zipDir(root, zipDir, zipOut);
+            zipOut.close();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
+    private static void zipDir(String root, File dir, ZipOutputStream zipOut)
+            throws IOException {
+        FileInputStream fileIn;
+        File[] files;
+        byte[] buf = new byte[1024];
+        int readedBytes;
+        files = dir.listFiles();
+
+        if (files.length == 0) {// 如果目录为空,则单独创建之.
+            zipOut.putNextEntry(new ZipEntry(dir.toString() + "/"));
+            zipOut.closeEntry();
+        } else {
+            for (File fileName : files) {
+                if (fileName.isDirectory()) {
+                    zipDir(root, fileName, zipOut);
+                } else {
+                    fileIn = new FileInputStream(fileName);
+                    ZipEntry zipEntry = new ZipEntry(new String(fileName.toURI()
+                            .getPath().replace(root, "").getBytes(), "GBK"));
+                    zipEntry.setUnixMode(755);
+                    zipOut.putNextEntry(zipEntry);
+
+                    while ((readedBytes = fileIn.read(buf)) > 0) {
+                        zipOut.write(buf, 0, readedBytes);
+                    }
+                    zipOut.closeEntry();
+                }
+            }
+        }
     }
 }
